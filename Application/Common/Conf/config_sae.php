e2416b7dab71b60086778707da43fbbf85c98389
momopluto <294666880@qq.com>
2015-05-08 21:42:11 +0800

    init project

---
+++ Application/Common/Conf/config_sae.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'TEMP_PARSE_STRING' => array(
+
+		'Uploads/rst_logo' => $st->getUrl('uploads', 'rst_logo');// 后面的uploads可能要改为public
+	)
+
+);
+
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+	define('APP_DEBUG', true);
+
+	// // 绑定Client模块到当前入口文件
+	// define('BIND_MODULE','Client');
+	// define('BUILD_CONTROLLER_LIST','Index,User,Order');
+	// define('BUILD_MODEL_LIST','User');
+
+    define('APP_PATH', './Application/');
+    define('THINK_PATH', './ThinkPHP/');
+
+    // define('DOMAIN_URL', "http://momopluto.xicp.net");//服务器域名
+    // define('DOMAIN_URL', "http://scauhometown.sinaapp.com");//服务器域名
+    
+    define('DOMAIN_URL', "http://127.0.0.1:8080");//服务器域名
+
+    define('PUBLIC_URL', '/platform/Application/Public');//Public公共文件夹路径
+    
+    define('ADMIN_SRC', '/platform/Application/Admin/Source');//Admin资源文件夹路径
+    define('HOME_SRC', '/platform/Application/Home/Source');//Home资源文件夹路径
+    define('CLIENT_SRC', '/platform/Application/CLient/Source');//CLient资源文件夹路径
+
+    define('ADMIN_TITLE', '订餐平台后台');
+    define('HOME_TITLE', '餐厅管理系统');
+    define('PLTF_NAME', '堂-订餐');
+
+    require THINK_PATH.'ThinkPHP.php'
+
+?>
