32e26756593a5112fca1cf662dbaf01bf3ddeff2
momo <294666880@qq.com>
2015-07-19 15:59:55 +0800

    删除Restaurant控制器下的测试代码

--- Application/Client/Controller/RestaurantController.class.php
+++ Application/Client/Controller/RestaurantController.class.php
@@ -3,40 +3,6 @@ namespace Client\Controller;
 use Think\Controller;
 class RestaurantController extends ClientController {
 
-
-    /**
-     * 测试传递JSON数据
-     */
-    function testJSON(){
-
-        $data = array();
-        $data['total'] = 50;
-        $data['item'][0] = array('name'=>"菜式1", 'price'=>10, 'count'=>2, 'total'=>20);
-        $data['item'][1] = array('name'=>"菜式2", 'price'=>10, 'count'=>3, 'total'=>30);
-        $data['note'] = "http://momopluto.xicp.net/platform/Application/Uploads/rst_logo/default_rst_logo.jpg";
-
-        $data = json_encode($data, JSON_UNESCAPED_UNICODE);// unicode格式
-
-        echo $data;
-        // p($_SERVER);
-    }
-
-    /**
-     * 测试餐厅的视图模型
-     */
-    function testRestaurantViewModel(){
-
-        $model = D("RestaurantView");
-        
-        $data = $model->select();
-        // p($model);
-        // p($data);
-
-        $data = json_encode($data, JSON_UNESCAPED_UNICODE);// unicode格式
-
-        echo $data;
-    }
-
     /**
      * 测试展示餐厅信息
      */
@@ -113,28 +79,6 @@ class RestaurantController extends ClientController {
 
         $this->display();
     }
-
-
-    // 进入餐厅选择页面前，取得用户token和openid，然后写入cookie
-    function checkUserValid(){
-
-        // $dst1 = "Client/Restaurant/lists";// I('get.dstUrl');
-        // $dst2 = "Client/Order/myOrder";// I('get.dstUrl');
-
-        $dst = I('get.dstUrl');
-        
-
-        $jump_url = U($dst);
-
-        $jump_url = "http://".$_SERVER['HTTP_HOST'].$jump_url;// 加上域名
-
-        // echo $jump_url;
-        // p($_SERVER);die;
-
-        get_zx_userid($jump_url);// 调用卓效获取用户user_id接口，会判断是否在微信打开
-    }
-
-
 
 
 // *******************提供查询餐厅状态接口
